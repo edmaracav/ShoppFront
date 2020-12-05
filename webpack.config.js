f8b4f7ea5fc6b21f94ed123a942795b9c6a07e8a
edmara <edmaracav@gmail.com>
2020-12-05 19:42:59 -0300

    Upd dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const { VueLoaderPlugin } = require("vue-loader");
-const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const autoprefixer = require("autoprefixer");
-
+const path = require("path");
 
 module.exports = {
   entry: {
     main: "./src/main.js",
   },
   output: {
+    filename: "[name].[contenthash:8].js",
     path: path.resolve(__dirname, "dist"),
+    chunkFilename: "[name].[contenthash:8].js",
   },
   module: {
     rules: [
@@ -23,21 +26,6 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: "vue-loader",
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
-        loader: "file-loader",
-        options: {
-          name: "[name][contenthash:8].[ext]",
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
-        loader: "file-loader",
-        options: {
-          outputPath: "assets",
-          esModule: false,
-        },
       },
       {
         test: /\.s?css$/,
@@ -54,16 +42,54 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name][contenthash:8].[ext]",
+          outputPath: "assets/img",
+          esModule: false,
+        },
+      },
     ],
   },
   plugins: [
     new VueLoaderPlugin(),
-    new MiniCssExtractPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css",
+      chunkFilename: "[name].[contenthash:8].css",
+    }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: "./public/favicon.ico",
+    }),
   ],
   resolve: {
     alias: {
       vue$: "vue/dist/vue.runtime.esm.js",
     },
     extensions: ["*", ".js", ".vue", ".json"],
+  },
+  optimization: {
+    moduleIds: "hashed",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          priority: -10,
+          chunks: "all",
+        },
+      },
+    },
+  },
+  devServer: {
+    historyApiFallback: true,
   },
 };
