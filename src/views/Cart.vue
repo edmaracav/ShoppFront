d515d9da3cb2abeeec10cd574ad183d02109d7b6
edmara <edmaracav@gmail.com>
2020-12-06 05:20:46 -0300

    Added cart page and app page

---
+++ src/views/Cart.vue
@@ -0,0 +1,19 @@
+  
+<template>
+  <div id="cart">
+    <img class="logo" src="../assets/logo.png" alt="logo" />
+  </div>
+</template>
+
+<script>
+
+import { Component, Vue } from "vue-property-decorator";
+import { Getter, Setter } from "vuex-class";
+
+class Cart extends Vue {
+
+}
+
+
+export default Cart;
+</script>
