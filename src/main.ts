149eca8e0fa512d339fad682e08ef7d08b3c65b4
edmara <edmaracav@gmail.com>
2020-12-06 17:12:28 -0300

    Updated main.ts with store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from './App.vue';
 import router from './router';
+import store from '../src/modules/index'
 
 new Vue({
+  store,
   router,
   render: (h) => h(App),
 }).$mount("#app");
