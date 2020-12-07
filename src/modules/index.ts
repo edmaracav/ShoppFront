0fda73d38c091a248a567440e84db2614a9b3ec4
edmara <edmaracav@gmail.com>
2020-12-07 00:20:50 -0300

    Add product module to index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import ProductModule from './ProductModule';
 import CartModule from './CartModule';
-
 
 Vue.use(Vuex);
 const store = new Vuex.Store({
     modules: {
+        ProductModule,
         CartModule
     }
 });
